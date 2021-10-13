cb0342894c8b9168ec30a7b50b97b75531842dd1
Victor Mcklaus <84514283+Mcklaus13@users.noreply.github.com>
2021-10-13 14:48:39 -0300

    Update Analise de Dados.py

--- Analise de Dados.py
+++ Analise de Dados.py
@@ -16,9 +16,7 @@ O que a empresa precisa fazer para resolver isso?
     
 Base de Dados: 
 https://drive.google.com/drive/folders/1T7D0BlWkNuy_MDpUHuBG44kT80EmRYIs?usp=sharing <br>
-
-Link Original do Kaggle: 
-https://www.kaggle.com/radmirzosimov/telecom-users-dataset'''
+'''
 
 
 # Passo 1: Entender qual é o problema
